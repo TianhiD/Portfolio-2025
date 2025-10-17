f5437130274ceae7d525f3439699d57b0a20bb90
Tianhi <Tianhi.Devold@stud.noroff.no>
2025-10-17 16:57:13 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Portfolio-2025/',
   plugins: [react()],
+  base: '/Portfolio-2025/',
 })
